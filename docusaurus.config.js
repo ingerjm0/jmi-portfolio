fd7f2d5fa20eb82e06cffa800e9f9621fdea613b
ingerjm0 <58995250+ingerjm0@users.noreply.github.com>
2025-10-22 15:53:06 -0700

    Add custom URL

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -15,10 +15,10 @@ const config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://ingerjm0.github.io',
+  url: 'https://jmidocs.com',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: 'jmi-portfolio',
+  baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
